ea97a62bcad3d06bf52f7a08359b0c8dd41dc925
marekgacek45 <marekgacek45@gmail.com>
2023-07-27 08:22:58 +0200

    style and add logic to post.blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,33 +1,13 @@
 <x-layout>
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1><a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}</a></h1>
-                    By <a href="/authors/{{$post->author->username}}"> {{$post->author->name}}</a> <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
-            <div>{{ $post->excerpt }}</div>
-        </article>
-    @endforeach --}}
-   @include('_posts-header')
+    @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-
-<x-post-featured-card/>
-
-        <div class="lg:grid lg:grid-cols-2">
-          
-            <x-post-card/>
-            <x-post-card/>
-           
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-
-<x-post-card/>
-<x-post-card/>
-<x-post-card/>
-
-        </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else{
+            <p>no posts yet.Please back later</p>
+            }
+        @endif
     </main>
 
-    <img src="{{asset('images/logo.svg')}}" alt="tutaj">
 </x-layout>
